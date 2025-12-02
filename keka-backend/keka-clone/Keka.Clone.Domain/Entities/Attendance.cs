c9a28d3e85980f879bfbd8c1db7f961d6bad05f1
saicharan8773 <saicharan8773@gmail.com>
2025-12-02 18:41:04 +0530

    Fixed attendance entity

--- keka-backend/keka-clone/Keka.Clone.Domain/Entities/Attendance.cs
+++ keka-backend/keka-clone/Keka.Clone.Domain/Entities/Attendance.cs
@@ -1,25 +1,14 @@
-using System;
-
-namespace Keka.Clone.Domain.Entities
+public class Attendance
 {
-    public class Attendance
-    {
-        public Guid Id { get; set; }
-
-        public Guid UserId { get; set; }
-
-        public DateTime Date { get; set; }
-
-        public DateTime? LoginTimeUtc { get; set; }
-        public double? LoginLat { get; set; }
-        public double? LoginLng { get; set; }
-
-        public DateTime? LogoutTimeUtc { get; set; }
-        public double? LogoutLat { get; set; }
-        public double? LogoutLng { get; set; }
-
-        public double? WorkedHours { get; set; }
-    }
+    public Guid Id { get; set; }
+    public Guid UserId { get; set; }
+    public DateTime Date { get; set; }
+    public DateTime? LoginTimeUtc { get; set; }
+    public DateTime? LogoutTimeUtc { get; set; }
+    public double? LoginLat { get; set; }
+    public double? LoginLng { get; set; }
+    public double? LogoutLat { get; set; }
+    public double? LogoutLng { get; set; }
+    public double? WorkedHours { get; set; }
+    public string? Notes { get; set; }
 }
-
-
